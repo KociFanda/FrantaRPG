4d7736fe6bdefa419269c418799f387c7b786afa
frantisek.koci <fanda.kom@gmail.com>
2022-03-30 13:57:39 +0200

    Animations for buttons and fuck go back arrow etc some shit

--- FrantaRPG/src/main/java/com/example/frantarpg/CharacterCreation.java
+++ FrantaRPG/src/main/java/com/example/frantarpg/CharacterCreation.java
@@ -1,5 +1,25 @@
 package com.example.frantarpg;
 
-public class CharacterCreation {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
 
+import java.io.IOException;
+
+public class CharacterCreation {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void goBack(MouseEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("main-menu.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
--- FrantaRPG/src/main/java/com/example/frantarpg/MainMenu.java
+++ FrantaRPG/src/main/java/com/example/frantarpg/MainMenu.java
@@ -6,28 +6,53 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import utills.LoadSave;
 
 import java.io.IOException;
 
 public class MainMenu {
+    public Button NewGameButton;
     private Stage stage;
     private Scene scene;
     private Parent root;
 
     @FXML
     public void onNewGameClick(ActionEvent event) throws IOException {
+
         root = FXMLLoader.load(getClass().getResource("character-creation.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+
     }
 
     public void LoadGame(ActionEvent actionEvent) {
         utills.LoadSave.load();
     }
-    
+
+    private double baseHeight;
+    private double baseWidht;
+    public void AnimationStart(MouseEvent mouseEvent) {
+        System.out.println("animation start");
+        Button button = (Button) mouseEvent.getSource();
+        double baseHeight = button.getMaxHeight();
+        double baseWidth = button.getMaxWidth();
+
+        button.setMaxWidth(baseWidth*0.7);
+        button.setMaxHeight(baseHeight*0.7);
+    }
+    public void AnimationEnd(MouseEvent mouseEvent) {
+        System.out.println("animation end");
+        Button button = (Button) mouseEvent.getSource();
+        this.baseHeight = baseHeight;
+        this.baseWidht  = baseWidht;
+
+        button.setMaxHeight(baseHeight);
+        button.setMaxWidth(baseWidht);
+   }
 }
